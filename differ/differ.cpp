5d45555ecf7a5ff04682195090dd657dd20416d6
Sergei Shulman <sgshulman@gmail.com>
2022-07-11 21:21:57 +0200

    Split differ logics into two functions

--- differ/differ.cpp
+++ differ/differ.cpp
@@ -39,7 +39,7 @@ class Image
             return correct_;
         }
 
-        double operator[](std::size_t i)
+        double operator[](std::size_t i) const
         {
             return i < data_.size() ? data_[i] : 0.0;
         }
@@ -83,17 +83,68 @@ class Image
         std::vector<double> data_;
 };
 
+struct DifferenceStats
+{
+    double absDifSum{ 0.0 };
+    double relDifSum{ 0.0 };
+    double totalSum{ 0.0 };
+    int pixelNum{ 0 };
+};
+
+DifferenceStats computeDifference(
+    Image const& image1,
+    Image const& image2,
+    std::ostream& difStream,
+    std::ostream& absDifStream,
+    std::ostream& relDifStream)
+{
+    DifferenceStats stats;
+    std::size_t i=0;
+    
+    for (std::int64_t x=0; x!=image1.rows(); ++x)
+    {
+        for (std::int64_t y=0; y!=image1.cols(); ++y)
+        {
+            double const dif = image1[i] - image2[i];
+            double const absDif = std::abs(dif);
+            double const mean = 0.5*(image1[i] + image2[i]);
+            difStream << dif << "\t";
+            absDifStream << absDif << "\t";
+
+            if (mean < 0.07)
+            {
+                stats.absDifSum += absDif;
+                stats.totalSum += mean;
+            }
+    
+            if (image1[i] > 0 && image2[i] > 0)
+            {
+                ++stats.pixelNum;
+                stats.relDifSum += absDif / mean;
+                relDifStream << absDif / mean << "\t";
+            } else {
+                relDifStream << "0\t";
+            }
+            ++i;
+        }
+        difStream << std::endl;
+        absDifStream << std::endl;
+        relDifStream << std::endl;
+    }
+    
+    return stats;
+}
 
 int main( int argc, char *argv[] )
 {
-	if (argc != 3)
-	{
-	    std::cerr << argv[0] << " requires 2 arguments." << std::endl;
-	    return 0;
-	}
+    if (argc != 3)
+    {
+        std::cerr << argv[0] << " requires 2 arguments." << std::endl;
+        return 0;
+    }
 
-	Image image1(argv[1]);
-	Image image2(argv[2]);
+    Image image1(argv[1]);
+    Image image2(argv[2]);
 
     if (!image1.correct() || !image2.correct())
     {
@@ -105,52 +156,20 @@ int main( int argc, char *argv[] )
         std::cerr << argv[1] << " (" << image1.rows() << "x" << image1.cols() << ") and " << argv[2] << " ("
                 << image2.rows() <<"x" << image2.cols() << ") should have the same shape." << std::endl;
 
-    	return 0;
+        return 0;
     }
 
-    std::ofstream difstream("dif.dat");
-	std::ofstream absdifstream("dif_abs.dat");
-	std::ofstream reldifstream("dif_rel.dat");
+    std::ofstream difStream("dif.dat");
+    std::ofstream absDifStream("dif_abs.dat");
+    std::ofstream relDifStream("dif_rel.dat");
 
-    difstream.precision(14);
-    absdifstream.precision(14);
-    reldifstream.precision(14);
+    difStream.precision(14);
+    absDifStream.precision(14);
+    relDifStream.precision(14);
 
-	double absdifsum=0.0, reldifsum=0.0, totsum=0;
+    DifferenceStats stats = computeDifference(image1, image2, difStream, absDifStream, relDifStream);
 
-	int num=0;
-	std::size_t i=0;
-
-	for (std::int64_t x=0; x!=image1.rows(); ++x)
-	{
-		for (std::int64_t y=0; y!=image1.cols(); ++y)
-		{
-			double const dif = image1[i] - image2[i];
-			double const absdif = std::abs(dif);
-            double const mean = 0.5*(image1[i] + image2[i]);
-			difstream << dif << "\t";
-			absdifstream << absdif << "\t";
-
-			if (mean < 0.07)
-			{
-				absdifsum += absdif;
-				totsum += mean;
-			}
-
-			if (image1[i] > 0 && image2[i] > 0)
-			{
-				++num;
-				reldifsum += absdif/mean;
-				reldifstream << absdif/mean << "\t";
-			} else {
-				reldifstream << "0\t";
-			}
-			++i;
-		}
-        difstream << std::endl;
-		absdifstream << std::endl;
-		reldifstream << std::endl;
-	}
-	std::cout << "dif= " << absdifsum << " meandif= " << absdifsum/num << " meanreldif= "
-	        << reldifsum/num << " n= " << num << std::endl << "refsum= " << absdifsum/totsum << std::endl;
+    std::cout << "dif= " << stats.absDifSum << " meandif= " << stats.absDifSum/stats.pixelNum << " meanreldif= "
+            << stats.relDifSum/stats.pixelNum << " n= " << stats.pixelNum << std::endl << "refsum= "
+            << stats.absDifSum/stats.totalSum << std::endl;
 }
