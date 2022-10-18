b320ec1b74c5d750daf1ef17ef8cc6be3da7c5aa
Sergei Shulman <sgshulman@gmail.com>
2022-10-18 22:23:05 +0200

    Increase MAX_DIMENSION of Sobol tables

--- source/Sobol.hpp
+++ source/Sobol.hpp
@@ -23,9 +23,9 @@ public:
     double Get() override;
     void Skip() override;
 private:
-    constexpr static unsigned int MAX_DIMENSION = 6;
+    constexpr static unsigned int MAX_DIMENSION = 15;
     constexpr static unsigned int POLINOMIAL_NUMBER = MAX_DIMENSION - 1;
-    constexpr static unsigned int MAX_DEGREE = 4;
+    constexpr static unsigned int MAX_DEGREE = 6;
     constexpr static unsigned int BIT_COUNT = 64;
     static double const POW2;
 
@@ -37,15 +37,31 @@ private:
     // p(z) = a_0 + a_1 z + a_2 z^2 + ... a_BIT_COUNT z^BIT_COUNT,
     // here a_i is the i-th bit of P_TABLE[j], j is the polynomial index.
     // our p_table element in binary form is 1(a_j from Joe and Kuo)1
-    constexpr static table_t P_TABLE[POLINOMIAL_NUMBER] = { 3, 7, 11, 13, 19 };
+    constexpr static table_t P_TABLE[POLINOMIAL_NUMBER] = {
+         3,  7, 11, 13, 19,
+        25, 37, 41, 47, 55,
+        59, 61, 67, 91};
 
     // initial m_i values for i=0..degree of the POLINOMIAL_NUMBER primitive polynomials
     constexpr static table_t MI_TABLE[MAX_DEGREE * POLINOMIAL_NUMBER] = {
-        1, 0, 0, 0,
-        1, 3, 0, 0,
-        1, 3, 1, 0,
-        1, 1, 1, 0,
-        1, 1, 3, 3
+        1,  0,  0,  0,  0,  0,
+        1,  3,  0,  0,  0,  0,
+
+        1,  3,  1,  0,  0,  0,
+        1,  1,  1,  0,  0,  0,
+
+        1,  1,  3,  3,  0,  0,
+        1,  3,  5, 13,  0,  0,
+
+        1,  1,  5,  5, 17,  0,
+        1,  1,  5,  5,  5,  0,
+        1,  1,  7, 11, 19,  0,
+        1,  1,  5,  1,  1,  0,
+        1,  1,  1,  3, 11,  0,
+        1,  3,  5,  5, 31,  0,
+
+        1,  3,  3,  9,  7, 49,
+        1,  1,  1, 15, 21, 21
     };
 
     static inline table_t m_initial(unsigned int dimension, unsigned int degree)
